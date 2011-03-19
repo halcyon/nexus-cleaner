f4306715136228df7e06f23651cea1397064cd5b
Scott McLeod <scott.mcleod@theice.com>
2011-03-18 21:46:05 -0400

    Reformatting

--- NexusCleaner.groovy
+++ NexusCleaner.groovy
@@ -10,98 +10,74 @@ import groovy.time.*
 
 class NexusCleaner {
 
-    def settings = [
-        baseUrl: 'http://localhost:8081/nexus/service/local/repositories/releases/content/'
-     ]
+  def settings = [
+      baseUrl: 'http://localhost:8081/nexus/service/local/repositories/releases/content/'
+    ]
 
 
-    def static main( def args )
+  def static main( def args )
+  {
+    if (args.length != 2)
     {
-      if (args.length != 2)
-      {
-        println "Usage: groovy NexusCleaner.groovy URI months"
-        System.exit(1)
-      }
-      def uri = args[0]
-      def months = args[1].toInteger()
-
-      if ( months < 1 )
-      {
-        println "Months must be greater than 0"
-        System.exit(1)
-      }
-      def nc = new NexusCleaner()
-      nc.findRelease(uri, months)
+      println "Usage: groovy NexusCleaner.groovy URI months"
+      System.exit(1)
     }
+    def uri = args[0]
+    def months = args[1].toInteger()
 
-    def findRelease( def uri, def months )
+    if ( months < 1 )
     {
-      def urls = scanRepo( settings.baseUrl+uri )
-      urls = urls.collect() { [it[0], new Date().parse('yyyy-mm-dd hh:mm:ss.S zzz',it[1])] }
-      println months
-
-      urls.each { println it }
-      use ( TimeCategory ) {
-            // application on numbers:
-//            println 1.minute.from.now
- //           println 10.days.ago
-            // application on dates
-  //          def someDate = new Date()
-   //         println someDate - 3.months 
-            urls.each{ println it[1] }
-      }
-
-
-
+      println "Months must be greater than 0"
+      System.exit(1)
     }
+    def nc = new NexusCleaner()
+    nc.findRelease(uri, months)
+  }
 
+  def findRelease( def uri, def months )
+  {
+    def urls = scanRepo( settings.baseUrl+uri )
+    urls = urls.collect() { [it[0], new Date().parse('yyyy-MM-dd HH:mm:ss.S zzz',it[1])] }
 
-    def findRC()
-    {
-
-        def urls = scanRepo( settings.baseUrl )
-
-        println urls
-
-        urls.findAll(){ it ==~ /.*-RC\d+\/$/ }.each() {
-            ver
-            // calculate what would be the final version
-            def rel = ver.replaceAll(/-RC\d+\/$/, '/' )
-            // check if the final version already exists
-            if( urls.find(){ it == rel } != null )
-                println ver
+    use ( TimeCategory ) {
+      urls.each
+      {
+        if (it[1] < months.months.ago)
+        {
+          println it[1]
+          println it[0]
         }
-        // delete irregular SNAPSHOTS ie: 1.0.2.0-SNAPSHOT2, 1.0.2.0-RC1SNAPSHOT2
-        urls.findAll(){ it ==~ /.*SNAPSHOT.*\//}.each(){println it }
-
+      }
     }
+  }
 
-    def scanRepo( def url ) {
-        def urls = []
-        def data = fetchContent( url )
-        data.data.'content-item'.each(){
-            item->
-            def name = item.text.text()
-            if( item.leaf.text() == 'false' )
+
+  def scanRepo( def url ) {
+    def urls = []
+    def data = fetchContent( url )
+    data.data.'content-item'.each(){
+        item->
+        def name = item.text.text()
+        if( item.leaf.text() == 'false' )
+        {
+            if(!( name ==~ /^\d+(\.\d+)*.*/ )) // it's a release number level
             {
-                if(!( name ==~ /^\d+(\.\d+)*.*/ )) // it's a release number level
-                {
-                    urls += scanRepo( item.resourceURI.text() )
-                }
-                else
-                {
-                    urls << [item.resourceURI.text(),item.lastModified.text()]
-                }
+                urls += scanRepo( item.resourceURI.text() )
+            }
+            else
+            {
+                urls << [item.resourceURI.text(),item.lastModified.text()]
             }
         }
-        return urls
     }
+    return urls
+  }
 
-    def fetchContent( String url )
-    {
-        def txt = new URL( url ).text
-        def recs = new XmlSlurper().parseText( txt )
-        return recs
-    }
+  def fetchContent( String url )
+  {
+    def txt = new URL( url ).text
+    def recs = new XmlSlurper().parseText( txt )
+    return recs
+  }
 
 }
