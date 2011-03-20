03a6b619494542b7fb5dab2db96be50381fd913e
Scott McLeod <scott.mcleod@theice.com>
2011-03-19 20:55:16 -0400

    Default to dryrun mode

--- NexusCleaner.groovy
+++ NexusCleaner.groovy
@@ -9,10 +9,11 @@ class NexusCleaner {
       curlCommand: 'curl -v -X DELETE -u admin:admin123'
     ]
 
+  def binding = new Binding()
 
   def static main(def args) {
     if (args.length < 3) {
-      println "Usage: ./NexusCleaner.groovy <nexusurl> <namespace> <months> [debug]"
+      println "Usage: ./NexusCleaner.groovy <nexusurl> <namespace> <months> [debug] [real]"
       println "e.g.: ./NexusCleaner.groovy http://localhost:8081/nexus com/theice 1 debug"
       System.exit(1)
     }
@@ -28,12 +29,15 @@ class NexusCleaner {
     def debug = false
     if (args.length > 3) debug = true
 
+    def real = false
+    if (args.length > 4) real = true
+
     def nc = new NexusCleaner()
     def url = nexusurl+settings.baseUri+namespace
-    nc.findRelease( url, months, debug)
+    nc.findRelease( url, months, debug, real)
   }
 
-  def findRelease(def url, def months, def debug) {
+  def findRelease(def url, def months, def debug, def real) {
     def urls = scanRepo(url)
     urls = urls.collect() { [it[0], new Date().parse('yyyy-MM-dd HH:mm:ss.S zzz',it[1])] }
 
@@ -41,13 +45,15 @@ class NexusCleaner {
       urls.each {
         if (it[1] < months.seconds.ago) {
           def command = settings.curlCommand + " " + it[0]
-//          def proc = command.execute()
-//          proc.waitFor()
           println command
-//          println "return code: ${ proc.exitValue()}"
-          if (debug) {
-            println "stderr: ${proc.err.text}"
-            println "stdout: ${proc.in.text}"
+          if (real) {
+            def proc = command.execute()
+            proc.waitFor()
+            println "return code: ${ proc.exitValue()}"
+            if (debug) {
+              println "stderr: ${proc.err.text}"
+              println "stdout: ${proc.in.text}"
+            }
           }
         }
       }
@@ -75,7 +81,7 @@ class NexusCleaner {
 
   def fetchContent(String url) {
     try {
-      def txt = new URL( url ).text
+      def txt = new URL(url).text
       def recs = new XmlSlurper().parseText( txt )
       return recs
     }
